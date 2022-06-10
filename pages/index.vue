c81a5acf2c3dde7c672368507d34583088446a6e
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-10 22:34:16 +0300

    feature: add home page without fix

--- pages/index.vue
+++ pages/index.vue
@@ -4,71 +4,103 @@ useTransition()
 </script>
 
 <template>
-  <div class="page">
-    <section class="section section--nm home">
-      <div class="container home__wrapper">
-        <div class="grid home__top-block">
-          <div class="home__elements-left">
-            <img class="home__big-img home__big-img--1" src="/images/building-1.jpg" alt="Building" />
-          </div>
-          <div class="home__elements-right">
-            <img class="home__big-img home__big-img--2" src="/images/building-2.jpg" alt="Building" />
-            <ul class="grid home__small-buildings">
-              <li class="home__li">
-                <p class="home__number">001</p>
-                <img
-                  class="home__small-img"
-                  src="/images/small-building-1.jpg"
-                  alt="Building"
-                />
-              </li>
-              <li class="home__li">
-                <p class="home__number">002</p>
-                <img
-                  class="home__small-img"
-                  src="/images/small-building-2.jpg"
-                  alt="Building"
-                />
-              </li>
-              <li class="home__li">
-                <p class="home__number">003</p>
-                <img
-                  class="home__small-img"
-                  src="/images/small-building-3.jpg"
-                  alt="Building"
-                />
-              </li>
-              <li class="home__li">
-                <p class="home__number">004</p>
-                <img
-                  class="home__small-img"
-                  src="/images/small-building-4.jpg"
-                  alt="Building"
-                />
-              </li>
-              <li class="home__li">
-                <p class="home__number">005</p>
-                <img
-                  class="home__small-img"
-                  src="/images/small-building-5.jpg"
-                  alt="Building"
-                />
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="grid home__bottom-block">
-          <p class="home__desc">
-            A New Norm To Design. Build. Think. powered by Dextall Studio, the
-            BIM Solution software
-          </p>
-          <span class="home__title">Unitized</span>
-          <span class="home__title">prefabricated</span>
-          <span class="home__title">exterior</span>
-          <span class="home__title">Wall</span>
-          <span class="home__title">system</span>
+  <section class="section section--nm home-1">
+    <div class="container home-1__wrapper">
+      <div class="grid home-1__top-block">
+        <div
+          class="big-img home-1__elements-left"
+          style="
+            background: url('/images/building-1.jpg');
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: cover;
+          "
+        ></div>
+        <div class="home-1__elements-right">
+          <div
+            class="big-img home-1__right-image"
+            style="
+              background-image: url('/images/building-2.jpg');
+              background-repeat: no-repeat;
+              background-position: center;
+              background-size: cover;
+            "
+          ></div>
+          <ul class="grid home-1__small-buildings">
+            <li class="home-1__li">
+              <p class="home-1__number">001</p>
+              <div
+                class="small-img home-1__small-img"
+                style="
+                  background-image: url('/images/small-building-1.jpg');
+                  background-repeat: no-repeat;
+                  background-position: center;
+                  background-size: cover;
+                "
+              ></div>
+            </li>
+            <li class="home-1__li">
+              <p class="home-1__number">002</p>
+              <div
+                class="small-img home-1__small-img"
+                style="
+                  background-image: url('/images/small-building-2.jpg');
+                  background-repeat: no-repeat;
+                  background-position: center;
+                  background-size: cover;
+                "
+              ></div>
+            </li>
+            <li class="home-1__li">
+              <p class="home-1__number">003</p>
+              <div
+                class="small-img home-1__small-img"
+                style="
+                  background-image: url('/images/small-building-3.jpg');
+                  background-repeat: no-repeat;
+                  background-position: center;
+                  background-size: cover;
+                "
+              ></div>
+            </li>
+            <li class="home-1__li">
+              <p class="home-1__number">004</p>
+              <div
+                class="small-img home-1__small-img"
+                style="
+                  background-image: url('/images/small-building-4.jpg');
+                  background-repeat: no-repeat;
+                  background-position: center;
+                  background-size: cover;
+                "
+              ></div>
+            </li>
+            <li class="home-1__li">
+              <p class="home-1__number">005</p>
+              <div
+                class="small-img home-1__small-img"
+                style="
+                  background-image: url('/images/small-building-5.jpg');
+                  background-repeat: no-repeat;
+                  background-position: center;
+                  background-size: cover;
+                "
+              ></div>
+            </li>
+          </ul>
         </div>
       </div>
-    </section>
-  </div>
+      <div class="grid home-1__bottom-block">
+        <span class="home-1__title">Unitized</span>
+        <span class="home-1__title">prefabricated</span>
+        <span class="home-1__title">exterior</span>
+        <span class="home-1__title">Wall</span>
+        <span class="home-1__title">system</span>
+        <p class="home-1__desc">
+          A New Norm To Design. Build. Think. powered by Dextall Studio, the BIM
+          Solution software
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
