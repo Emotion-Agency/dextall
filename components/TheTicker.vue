0e81403f002ea9cd7b2c0b3d30d3a025aa915d41
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-13 17:49:58 +0300

    feature: add ticker without js

---
+++ components/TheTicker.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="ticker">
+    <div class="ticker__wrapper">
+      <div class="ticker__item">
+        A New Norm in The Construction Process — A New Norm in The Construction
+        Process
+      </div>
+      <div class="ticker__item">
+        A New Norm in The Construction Process — A New Norm in The Construction
+        Process
+      </div>
+    </div>
+  </div>
+</template>
