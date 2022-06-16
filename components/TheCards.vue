7a50fa365ae64d94825372ba94cd4549d1fb6f0c
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-16 18:28:58 +0300

    feature: add icon to ticker

--- components/TheCards.vue
+++ components/TheCards.vue
@@ -17,5 +17,39 @@
         <img class="cards__img" src="/images/home/16.jpg" alt="Building" />
       </div>
     </li>
+    <li class="grid cards__li">
+      <div class="cards__text-block">
+        <h3 class="cards__title">Article 1</h3>
+        <p class="cards__desc">
+          We cant wait to see the building ready! And since its build with our
+          DWall 2000, the building process will be fast. Here are the details:
+        </p>
+        <CircleButton class="cards__btn" />
+      </div>
+      <div class="cards__line-block">
+        <p class="cards__date">17.05.2022</p>
+        <div class="cards__line"></div>
+      </div>
+      <div class="cards__image-block">
+        <img class="cards__img" src="/images/home/16.jpg" alt="Building" />
+      </div>
+    </li>
+    <li class="grid cards__li">
+      <div class="cards__text-block">
+        <h3 class="cards__title">Article 1</h3>
+        <p class="cards__desc">
+          We cant wait to see the building ready! And since its build with our
+          DWall 2000, the building process will be fast. Here are the details:
+        </p>
+        <CircleButton class="cards__btn" />
+      </div>
+      <div class="cards__line-block">
+        <p class="cards__date">17.05.2022</p>
+        <div class="cards__line"></div>
+      </div>
+      <div class="cards__image-block">
+        <img class="cards__img" src="/images/home/16.jpg" alt="Building" />
+      </div>
+    </li>
   </ul>
 </template>
--- components/TheTicker.vue
+++ components/TheTicker.vue
@@ -2,12 +2,18 @@
   <div class="ticker">
     <div class="ticker__wrapper">
       <div class="ticker__item">
-        A New Norm in The Construction Process — A New Norm in The Construction
-        Process
+        A New Norm in The Construction Process
+        <span class="ticker__icon-wrapper">
+          <IconsTickerIcon class="ticker__icon" />
+        </span>
+        A New Norm in The Construction Process
       </div>
       <div class="ticker__item">
-        A New Norm in The Construction Process — A New Norm in The Construction
-        Process
+        A New Norm in The Construction Process
+        <span class="ticker__icon-wrapper">
+          <IconsTickerIcon class="ticker__icon" />
+        </span>
+        A New Norm in The Construction Process
       </div>
     </div>
   </div>
