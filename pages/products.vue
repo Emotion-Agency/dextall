965a7c3462dff3cc9c17f3037d74ac34df1c1759
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-17 22:29:02 +0300

    feature: add product-1 without fix

---
+++ pages/products.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="section section--nm products-1">
+    <div class="container products-1__wrapper">
+      <div class="grid products-1__image-block">
+        <div
+          class="products-1__img"
+          style="background-image: url('/images/products/1.jpg')"
+        ></div>
+        <div
+          class="products-1__img"
+          style="background-image: url('/images/products/2.jpg')"
+        ></div>
+        <div
+          class="products-1__img"
+          style="background-image: url('/images/products/3.jpg')"
+        ></div>
+        <div
+          class="products-1__img"
+          style="background-image: url('/images/products/4.jpg')"
+        ></div>
+      </div>
+      <div class="products-1__text-block">
+        <h1 class="products-1__title">
+          <span class="products-1__span-title">D Wall</span>
+          <span class="products-1__span-title"> non-load</span>
+          <span class="products-1__span-title"> Bearing System</span>
+        </h1>
+        <p class="products-1__desc">
+          Our products are designed for new and retrofit construction
+        </p>
+        <CircleButton class="products-1__btn"> Show products </CircleButton>
+      </div>
+    </div>
+  </section>
+  <section class="section section--nm products-1">
+    <div class="container products-1__wrapper"></div>
+  </section>
+</template>
