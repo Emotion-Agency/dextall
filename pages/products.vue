bf947b5e4921957382740c505459c5212d15dc63
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-18 10:16:35 +0300

    feature: complete products-3 without fix

--- pages/products.vue
+++ pages/products.vue
@@ -55,6 +55,80 @@
     </div>
   </section>
   <section class="section products-3">
-    <div class="container products-3__wrapper"></div>
+    <div class="container products-3__wrapper">
+      <p class="products-3__commentary">Simple, efficient, optimized</p>
+      <div class="products-3__line"></div>
+      <TheTicker class="products-3__ticker" />
+      <div class="grid products-3__content">
+        <ul class="products-3__list">
+          <li class="products-3__li">
+            <div class="products-3__line"></div>
+            <div class="products-3__text-wrapper">
+              <p class="products-3__number">01</p>
+              <p class="products-3__text">
+                Non Combustible stone wool insulation
+              </p>
+            </div>
+          </li>
+          <li class="products-3__li">
+            <div class="products-3__line"></div>
+            <div class="products-3__text-wrapper">
+              <p class="products-3__number">02</p>
+              <p class="products-3__text">
+                High thermal and acoustical performance steel reinforced polymer
+                windows
+              </p>
+            </div>
+          </li>
+          <li class="products-3__li">
+            <div class="products-3__line"></div>
+            <div class="products-3__text-wrapper">
+              <p class="products-3__number">03</p>
+              <p class="products-3__text">Rain Screen Cladding</p>
+            </div>
+          </li>
+          <li class="products-3__li">
+            <div class="products-3__line"></div>
+            <div class="products-3__text-wrapper">
+              <p class="products-3__number">04</p>
+              <p class="products-3__text">Sheet metal vapor barrier</p>
+            </div>
+          </li>
+          <li class="products-3__li">
+            <div class="products-3__line"></div>
+            <div class="products-3__text-wrapper">
+              <p class="products-3__number">05</p>
+              <p class="products-3__text">
+                High performance gaskets for a dry connection
+              </p>
+            </div>
+          </li>
+          <li class="products-3__li">
+            <div class="products-3__line"></div>
+            <div class="products-3__text-wrapper">
+              <p class="products-3__number">06</p>
+              <p class="products-3__text">
+                Non-combustible vapor permeable sheathing, includes weather and
+                water protection layer
+              </p>
+            </div>
+            <div class="products-3__line"></div>
+          </li>
+        </ul>
+        <div class="products-3__image-wrapper">
+          <img
+            class="products-3__img"
+            src="/images/products/5.jpg"
+            alt="Building"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="section products-4">
+    <div class="container products-4__wrapper"></div>
+  </section>
+    <section class="section products-5">
+    <div class="container products-5__wrapper"></div>
   </section>
 </template>
