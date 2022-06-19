3d846a9e5feb6288997cc21654143079cdb3320f
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-19 16:38:24 +0300

    feature: bim-4 section complete without fix

--- pages/bim.vue
+++ pages/bim.vue
@@ -117,6 +117,84 @@
     </div>
   </section>
   <section class="section bim-4">
-    <div class="container bim-4__wrapper"></div>
+    <div class="container bim-4__wrapper">
+      <h2 class="bim-4__title">Results</h2>
+      <ul class="bim-4__list">
+        <li class="bim-4__li">
+          <div class="bim-4__line"></div>
+          <div class="grid bim-4__content-wrapper">
+            <div class="bim-4__text-wrapper">
+              <h3 class="bim-4__content-title">For Architects</h3>
+              <p class="bim-4__content-desc">
+                Optimize with flexibility and accuracy
+              </p>
+            </div>
+            <ul class="bim-4__content-list">
+              <li class="bim-4__content-li">
+                <div class="bim-4__mob-line"></div>
+                <p class="bim-4__content-text bim-4__content-text--nm">
+                  Less mistakes means more time to fuel your creativity
+                </p>
+                <div class="bim-4__line"></div>
+              </li>
+              <li class="bim-4__content-li">
+                <p class="bim-4__content-text">
+                  Faster design process and real-time exterior skin cost updates
+                </p>
+                <div class="bim-4__line"></div>
+              </li>
+              <li class="bim-4__content-li">
+                <p class="bim-4__content-text">
+                  Fully optimized to work with retrofit projects in Revit
+                </p>
+                <div class="bim-4__line"></div>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li class="bim-4__li">
+          <div class="bim-4__line"></div>
+          <div class="grid bim-4__content-wrapper">
+            <div class="bim-4__text-wrapper">
+              <h3 class="bim-4__content-title">
+                For GCs and construction managers
+              </h3>
+              <p class="bim-4__content-desc">
+                Easily obtain installation manuals and 3D high detailed
+                renderings
+              </p>
+            </div>
+            <ul class="bim-4__content-list">
+              <li class="bim-4__content-li">
+                <div class="bim-4__mob-line"></div>
+                <p class="bim-4__content-text bim-4__content-text--nm">
+                  Less time spend installing and lower overall cost
+                </p>
+                <div class="bim-4__line"></div>
+              </li>
+              <li class="bim-4__content-li">
+                <p class="bim-4__content-text">
+                  Less mistakes during installations
+                </p>
+                <div class="bim-4__line"></div>
+              </li>
+              <li class="bim-4__content-li">
+                <p class="bim-4__content-text">
+                  Fully optimized to work with retrofit projects on site
+                </p>
+                <div class="bim-4__line"></div>
+                <p class="bim-4__about-text">
+                  Support: video chat support.<br />
+                  Education: training on plug-in options
+                </p>
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+  <section class="section bim-5">
+    <div class="container bim-5__wrapper"></div>
   </section>
 </template>
