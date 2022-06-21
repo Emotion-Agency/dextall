0455894ad551f5876674c8f8959c6b116925ce6a
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-21 23:10:27 +0300

    feature: complete project-1 without fix

---
+++ pages/project.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="section section--nm project-1">
+    <div
+      class="project-1__bg"
+      style="background-image: url('/images/projects/project/1.jpg')"
+    ></div>
+    <div class="container project-1__wrapper">
+      <div class="project-1__text-block">
+        <p class="project-1__text">Minimized Tenant Disruption</p>
+        <h1 class="project-1__title">
+          <span class="project-1__span-title">The</span>
+          <span class="project-1__span-title"> Heritage</span>
+        </h1>
+      </div>
+      <p class="project-1__desc">
+        A “hybrid” renovation for 1974 building to increase comfort for the
+        tenants, reduce the buildings energy consumption, and offer a
+        transformative modern appearance
+      </p>
+    </div>
+  </section>
+</template>
