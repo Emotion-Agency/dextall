2a7f7db4d2d10989e3de44d039f42c801bd73b8c
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-21 10:59:42 +0300

    feature: about-3, about-4 section complete

--- pages/about.vue
+++ pages/about.vue
@@ -25,6 +25,40 @@
     </div>
   </section>
   <section class="section about-3">
-    <div class="container about-3__wrapper"></div>
+    <div
+      class="about-3__bg"
+      style="background-image: url('/images/about/5.jpg')"
+    ></div>
+    <div class="container about-3__wrapper">
+      <div class="grid about-3__text">
+        <h3 class="about-3__title">
+          Sustainable Practicies should not be a luxury, they should be part of
+          our everyday lives
+        </h3>
+        <p class="about-3__desc">
+          At Dextall, we focus on creating long term economic, social, and
+          environmental value for our clients. Our team works hand in hand with
+          architects, contractors, and owners to ensure sustainability is
+          imprinted throughout the design and installation process of our
+          panelized wall system.
+        </p>
+        <p class="about-3__desc">
+          We believe technological advancements should be accessible and
+          affordable to all if we are to battle climate change. With decades of
+          experience, our team's sole goal is to set a new norm in the
+          construction industry for building better, more efficient, and
+          affordable buildings for our communities
+        </p>
+      </div>
+    </div>
+  </section>
+  <section class="section about-4">
+        <div class="container about-4__wrapper">
+      <div class="about-4__line"></div>
+      <h3 class="about-4__title">
+Approximately 16 trillion dollars of new buildings are built worldwide every year. We want to ensure that those buildings exceed local, state, and national environmental regulations while providing a healthy lifestyle for their occupants
+      </h3>
+      <div class="about-4__line"></div>
+    </div>
   </section>
 </template>
