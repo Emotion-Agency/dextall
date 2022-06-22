3382cae950e965c9c5f53860890309f8a006246c
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-22 12:34:35 +0300

    feature: library page full complete

---
+++ pages/library.vue
@@ -0,0 +1,109 @@
+<template>
+  <section class="section section--nm library-1">
+    <div class="container library-1__wrapper">
+      <h1 class="library-1__title">Dextall Studio Growing Features list</h1>
+    </div>
+  </section>
+  <section class="section library-2">
+    <div class="container library-2__wrapper">
+      <ul class="grid library-2__img-wrapper">
+        <li class="library-2__img-li">
+          <div class="library-2__text-wrapper">
+            <p class="library-2__name">News name</p>
+            <p class="library-2__date">03.06.22</p>
+          </div>
+          <img
+            class="library-2__img"
+            src="/images/library/1.jpg"
+            alt="Building"
+          />
+          <p class="library-2__desc">
+            Dextall's purpose: To accelerate decarbonization of the environment
+            TODAY by bringing affordable and efficient solutions to build
+            sustainably
+          </p>
+        </li>
+        <li class="library-2__img-li">
+          <div class="library-2__text-wrapper">
+            <p class="library-2__name">News name</p>
+            <p class="library-2__date">03.06.22</p>
+          </div>
+          <img
+            class="library-2__img"
+            src="/images/library/2.jpg"
+            alt="Building"
+          />
+          <p class="library-2__desc">
+            Elementum tellus augue auctor dolor. Hendrerit nec eget hac in nunc,
+            non ut integer netus. Pulvinar sit gravida quam cras eget quisque
+            orci. Mi metus nec massa aliquam volutpat a.
+          </p>
+        </li>
+        <li class="library-2__img-li">
+          <div class="library-2__text-wrapper">
+            <p class="library-2__name">News name</p>
+            <p class="library-2__date">03.06.22</p>
+          </div>
+          <img
+            class="library-2__img"
+            src="/images/library/3.jpg"
+            alt="Building"
+          />
+          <p class="library-2__desc">
+            Placerat tincidunt nisi ullamcorper varius feugiat ultrices dapibus
+            vel. Leo a orci convallis id. Nec nunc vehicula risus, dictumst
+            donec at accumsan fusce. Aliquam iaculis pharetra dis.
+          </p>
+        </li>
+        <li class="library-2__img-li">
+          <div class="library-2__text-wrapper">
+            <p class="library-2__name">News name</p>
+            <p class="library-2__date">03.06.22</p>
+          </div>
+          <img
+            class="library-2__img"
+            src="/images/library/4.jpg"
+            alt="Building"
+          />
+          <p class="library-2__desc">
+            Eget ut eu eget sed in quis dictum posuere. Pellentesque ut ante
+            facilisis magna feugiat elementum. Sagittis aliquet urna vel cras
+            tellus proin est. Tristique libero sit nec diam eget.
+          </p>
+        </li>
+        <li class="library-2__img-li">
+          <div class="library-2__text-wrapper">
+            <p class="library-2__name">News name</p>
+            <p class="library-2__date">03.06.22</p>
+          </div>
+          <img
+            class="library-2__img"
+            src="/images/library/5.jpg"
+            alt="Building"
+          />
+          <p class="library-2__desc">
+            Sagittis dui et magna bibendum donec sed natoque. Vestibulum, arcu
+            enim tellus fermentum lacus. A, nulla quis risus dignissim lorem
+            faucibus nibh. Risus dignissim neque.
+          </p>
+        </li>
+        <li class="library-2__img-li">
+          <div class="library-2__text-wrapper">
+            <p class="library-2__name">News name</p>
+            <p class="library-2__date">03.06.22</p>
+          </div>
+          <img
+            class="library-2__img"
+            src="/images/library/6.jpg"
+            alt="Building"
+          />
+          <p class="library-2__desc">
+            Consectetur purus phasellus quisque et ullamcorper. Urna, etiam nec
+            eget enim tempor. Est et enim cursus scelerisque felis euismod. Sit
+            sem in non ultricies ante viverra.
+          </p>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
