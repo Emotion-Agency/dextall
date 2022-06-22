4eddbffe7b8842723c7af73b2294002bf4e64ed2
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-22 18:29:01 +0300

    fix: update careers-5 section

--- pages/careers.vue
+++ pages/careers.vue
@@ -51,4 +51,97 @@
       </div>
     </div>
   </section>
+  <section class="section careers-4">
+    <div class="container careers-4__wrapper">
+      <div class="careers-4__line"></div>
+      <div class="careers-4__text-wrapper">
+        <h3 class="careers-4__title">We strive to always raise the bar</h3>
+        <p class="careers-4__text">
+          Dextall’s leaders hire other leaders and think “I must hire the
+          candidate that will most likely outperform me”.
+          <br />
+          <br />
+          Elevating our mission is the primary goal of each team member
+        </p>
+      </div>
+      <div class="careers-4__line"></div>
+    </div>
+  </section>
+  <section class="section careers-5">
+    <div class="container careers-5__wrapper">
+      <div class="careers-5__title-wrapper">
+        <h2 class="careers-5__title">Are you ready to change the world?</h2>
+        <p class="careers-5__desc">
+          If you’re interested in working with us check out below our current
+          openings. If you don’t see an exact fit but you feel inspired by our
+          work drop us a note, portfolio link, or résumé
+        </p>
+      </div>
+      <ul class="careers-5__filter-list">
+        <li class="careers-5__filter-li">
+          <button class="careers-5__filter-name">All departments</button>
+          <div class="careers-5__filter-numbers">(3)</div>
+        </li>
+        <li class="careers-5__filter-li">
+          <button class="careers-5__filter-name">Project execution</button>
+          <p class="careers-5__filter-numbers">(1)</p>
+        </li>
+        <li class="careers-5__filter-li">
+          <button class="careers-5__filter-name">Marketing and pr</button>
+          <p class="careers-5__filter-numbers">(1)</p>
+        </li>
+        <li class="careers-5__filter-li">
+          <button class="careers-5__filter-name">Engineering</button>
+          <p class="careers-5__filter-numbers">(1)</p>
+        </li>
+      </ul>
+      <ul class="careers-5__content-list">
+        <li class="careers-5__content-li">
+          <div class="careers-5__line"></div>
+          <div class="grid careers-5__content">
+            <p class="careers-5__number">01</p>
+            <div class="careers-5__text-wrapper">
+              <h3 class="careers-5__content-title">Project Manager – USA</h3>
+              <div class="careers-5__text-block">
+                <p class="careers-5__text">
+                  • Support the team in the design of digital strategies<br />
+                  • Collect information, analyze needs, study best practices
+                  (benchmarking), analyze visit trends, identify performance
+                  indicators (KPIs) <br />
+                  • Actively participate in the conversation in order to create
+                  an effective tree structure and wireframe.<br />
+                  • Monitor new practices and trends
+                </p>
+              </div>
+            </div>
+            <IconsArrowUp class="careers-5__arrow" />
+          </div>
+        </li>
+        <li class="careers-5__content-li">
+          <div class="careers-5__line"></div>
+          <div class="grid careers-5__content">
+            <p class="careers-5__number">02</p>
+            <div class="careers-5__text-wrapper">
+              <h3 class="careers-5__content-title">Social Media Internship</h3>
+            </div>
+            <IconsArrowDown class="careers-5__arrow" />
+          </div>
+        </li>
+        <li class="careers-5__content-li">
+          <div class="careers-5__line"></div>
+          <div class="grid careers-5__content">
+            <p class="careers-5__number">03</p>
+            <div class="careers-5__text-wrapper">
+              <h3 class="careers-5__content-title">
+                3D Designer, sketcher for high-tech sustainable construction
+                team
+              </h3>
+            </div>
+            <IconsArrowDown class="careers-5__arrow" />
+          </div>
+          <div class="careers-5__line"></div>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
