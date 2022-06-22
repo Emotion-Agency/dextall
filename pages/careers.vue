cae0a7de5246e5e5ed5a913b99c97877ffcccfd1
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-22 13:12:08 +0300

    feature: careers-3 withou fix complete

--- pages/careers.vue
+++ pages/careers.vue
@@ -22,4 +22,33 @@
       </h2>
     </div>
   </section>
+  <section class="section careers-3">
+    <div class="container grid careers-3__wrapper">
+      <div class="careers-3__text-block">
+        <h2 class="careers-3__big-text">Our mission is clear:</h2>
+        <p class="careers-3__desc">
+          “To accelerate decarbonization of the environment TODAY by bringing
+          affordable and efficient solutions to build sustainably”
+        </p>
+        <p class="careers-3__small-text">
+          To fight climate change, we must act urgently. To achieve that, we
+          hire only the best!
+        </p>
+      </div>
+      <div class="careers-3__image-top">
+        <img
+          class="careers-3__img"
+          src="/images/careers/2.jpg"
+          alt="Building"
+        />
+      </div>
+      <div class="careers-3__image-bottom">
+        <img
+          class="careers-3__img"
+          src="/images/careers/3.jpg"
+          alt="Building"
+        />
+      </div>
+    </div>
+  </section>
 </template>
