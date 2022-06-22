69ac8b4d95760c4531542a6450632d4eb4b2d5a9
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-22 12:55:19 +0300

    careers-2 section complete

---
+++ pages/careers.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="section section--nm careers-1">
+    <div class="container careers-1__wrapper">
+      <div class="careers-1__text-block">
+        <h1 class="careers-1__title">Careers</h1>
+      </div>
+      <p class="careers-1__desc">
+        Be part of decarbonizing our world. Set The New Normal. Work in a cool
+        team
+      </p>
+    </div>
+  </section>
+  <section class="section careers-2">
+    <div
+      class="careers-2__bg"
+      style="background-image: url('/images/careers/1.jpg')"
+    ></div>
+    <div class="container grid careers-2__wrapper">
+      <h2 class="careers-2__title">
+        We are always on the lookout for talented people that use their skills
+        for the greater good
+      </h2>
+    </div>
+  </section>
+</template>
