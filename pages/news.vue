8f719bf1679f273b6a21d0e9b42545ba2401eedf
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-23 12:43:46 +0300

    fix: add links to news and library sections

--- pages/news.vue
+++ pages/news.vue
@@ -8,76 +8,88 @@
     <div class="container news-2__wrapper">
       <ul class="grid news-2__img-wrapper">
         <li class="news-2__img-li">
-          <div class="news-2__text-wrapper">
-            <p class="news-2__name">News name</p>
-            <p class="news-2__date">03.06.22</p>
-          </div>
-          <img class="news-2__img" src="/images/news/1.jpg" alt="Building" />
-          <p class="news-2__desc">
-            Dextall's purpose: To accelerate decarbonization of the environment
-            TODAY by bringing affordable and efficient solutions to build
-            sustainably
-          </p>
+          <NuxtLink to="/" class="news-2__link">
+            <div class="news-2__text-wrapper">
+              <p class="news-2__name">News name</p>
+              <p class="news-2__date">03.06.22</p>
+            </div>
+            <img class="news-2__img" src="/images/news/1.jpg" alt="Building" />
+            <p class="news-2__desc">
+              Dextall's purpose: To accelerate decarbonization of the
+              environment TODAY by bringing affordable and efficient solutions
+              to build sustainably
+            </p>
+          </NuxtLink>
         </li>
         <li class="news-2__img-li">
-          <div class="news-2__text-wrapper">
-            <p class="news-2__name">News name</p>
-            <p class="news-2__date">03.06.22</p>
-          </div>
-          <img class="news-2__img" src="/images/news/2.jpg" alt="Building" />
-          <p class="news-2__desc">
-            Elementum tellus augue auctor dolor. Hendrerit nec eget hac in nunc,
-            non ut integer netus. Pulvinar sit gravida quam cras eget quisque
-            orci. Mi metus nec massa aliquam volutpat a.
-          </p>
+          <NuxtLink to="/" class="news-2__link">
+            <div class="news-2__text-wrapper">
+              <p class="news-2__name">News name</p>
+              <p class="news-2__date">03.06.22</p>
+            </div>
+            <img class="news-2__img" src="/images/news/2.jpg" alt="Building" />
+            <p class="news-2__desc">
+              Elementum tellus augue auctor dolor. Hendrerit nec eget hac in
+              nunc, non ut integer netus. Pulvinar sit gravida quam cras eget
+              quisque orci. Mi metus nec massa aliquam volutpat a.
+            </p>
+          </NuxtLink>
         </li>
         <li class="news-2__img-li">
-          <div class="news-2__text-wrapper">
-            <p class="news-2__name">News name</p>
-            <p class="news-2__date">03.06.22</p>
-          </div>
-          <img class="news-2__img" src="/images/news/3.jpg" alt="Building" />
-          <p class="news-2__desc">
-            Placerat tincidunt nisi ullamcorper varius feugiat ultrices dapibus
-            vel. Leo a orci convallis id. Nec nunc vehicula risus, dictumst
-            donec at accumsan fusce. Aliquam iaculis pharetra dis.
-          </p>
+          <NuxtLink to="/" class="news-2__link">
+            <div class="news-2__text-wrapper">
+              <p class="news-2__name">News name</p>
+              <p class="news-2__date">03.06.22</p>
+            </div>
+            <img class="news-2__img" src="/images/news/3.jpg" alt="Building" />
+            <p class="news-2__desc">
+              Placerat tincidunt nisi ullamcorper varius feugiat ultrices
+              dapibus vel. Leo a orci convallis id. Nec nunc vehicula risus,
+              dictumst donec at accumsan fusce. Aliquam iaculis pharetra dis.
+            </p>
+          </NuxtLink>
         </li>
         <li class="news-2__img-li">
-          <div class="news-2__text-wrapper">
-            <p class="news-2__name">News name</p>
-            <p class="news-2__date">03.06.22</p>
-          </div>
-          <img class="news-2__img" src="/images/news/4.jpg" alt="Building" />
-          <p class="news-2__desc">
-            Eget ut eu eget sed in quis dictum posuere. Pellentesque ut ante
-            facilisis magna feugiat elementum. Sagittis aliquet urna vel cras
-            tellus proin est. Tristique libero sit nec diam eget.
-          </p>
+          <NuxtLink to="/" class="news-2__link">
+            <div class="news-2__text-wrapper">
+              <p class="news-2__name">News name</p>
+              <p class="news-2__date">03.06.22</p>
+            </div>
+            <img class="news-2__img" src="/images/news/4.jpg" alt="Building" />
+            <p class="news-2__desc">
+              Eget ut eu eget sed in quis dictum posuere. Pellentesque ut ante
+              facilisis magna feugiat elementum. Sagittis aliquet urna vel cras
+              tellus proin est. Tristique libero sit nec diam eget.
+            </p>
+          </NuxtLink>
         </li>
         <li class="news-2__img-li">
-          <div class="news-2__text-wrapper">
-            <p class="news-2__name">News name</p>
-            <p class="news-2__date">03.06.22</p>
-          </div>
-          <img class="news-2__img" src="/images/news/5.jpg" alt="Building" />
-          <p class="news-2__desc">
-            Sagittis dui et magna bibendum donec sed natoque. Vestibulum, arcu
-            enim tellus fermentum lacus. A, nulla quis risus dignissim lorem
-            faucibus nibh. Risus dignissim neque.
-          </p>
+          <NuxtLink to="/" class="news-2__link">
+            <div class="news-2__text-wrapper">
+              <p class="news-2__name">News name</p>
+              <p class="news-2__date">03.06.22</p>
+            </div>
+            <img class="news-2__img" src="/images/news/5.jpg" alt="Building" />
+            <p class="news-2__desc">
+              Sagittis dui et magna bibendum donec sed natoque. Vestibulum, arcu
+              enim tellus fermentum lacus. A, nulla quis risus dignissim lorem
+              faucibus nibh. Risus dignissim neque.
+            </p>
+          </NuxtLink>
         </li>
         <li class="news-2__img-li">
-          <div class="news-2__text-wrapper">
-            <p class="news-2__name">News name</p>
-            <p class="news-2__date">03.06.22</p>
-          </div>
-          <img class="news-2__img" src="/images/news/6.jpg" alt="Building" />
-          <p class="news-2__desc">
-            Consectetur purus phasellus quisque et ullamcorper. Urna, etiam nec
-            eget enim tempor. Est et enim cursus scelerisque felis euismod. Sit
-            sem in non ultricies ante viverra.
-          </p>
+          <NuxtLink to="/" class="news-2__link">
+            <div class="news-2__text-wrapper">
+              <p class="news-2__name">News name</p>
+              <p class="news-2__date">03.06.22</p>
+            </div>
+            <img class="news-2__img" src="/images/news/6.jpg" alt="Building" />
+            <p class="news-2__desc">
+              Consectetur purus phasellus quisque et ullamcorper. Urna, etiam
+              nec eget enim tempor. Est et enim cursus scelerisque felis
+              euismod. Sit sem in non ultricies ante viverra.
+            </p>
+          </NuxtLink>
         </li>
       </ul>
     </div>
