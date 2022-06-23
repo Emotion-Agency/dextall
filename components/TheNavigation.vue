f8dd6753eae9e70556a6dc50d54f475f868f0a14
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-23 17:41:46 +0300

    fix: update image

--- components/TheNavigation.vue
+++ components/TheNavigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navigation">
+  <div class="navigation navigation--open">
     <div class="navigation__left-block">
       <div
         class="navigation__img"
@@ -14,38 +14,23 @@
       <ul class="navigation__list">
         <li class="navigation__li">
           <div class="navigation__line"></div>
-          <a
-            href="#"
-            class="navigation__link"
-          > Home </a>
+          <a href="#" class="navigation__link"> Home </a>
         </li>
         <li class="navigation__li">
           <div class="navigation__line"></div>
-          <NuxtLink
-            to="/"
-            class="navigation__link"
-          > Products </NuxtLink>
+          <NuxtLink to="/" class="navigation__link"> Products </NuxtLink>
         </li>
         <li class="navigation__li">
           <div class="navigation__line"></div>
-          <NuxtLink
-            to="/"
-            class="navigation__link"
-          > BIM Solution </NuxtLink>
+          <NuxtLink to="/" class="navigation__link"> BIM Solution </NuxtLink>
         </li>
         <li class="navigation__li">
           <div class="navigation__line"></div>
-          <NuxtLink
-            to="/"
-            class="navigation__link"
-          > Projects </NuxtLink>
+          <NuxtLink to="/" class="navigation__link"> Projects </NuxtLink>
         </li>
         <li class="navigation__li">
           <div class="navigation__line"></div>
-          <NuxtLink
-            to="/"
-            class="navigation__link"
-          > News </NuxtLink>
+          <NuxtLink to="/" class="navigation__link"> News </NuxtLink>
           <div class="navigation__line"></div>
         </li>
       </ul>
@@ -54,33 +39,21 @@
         <ul class="navigation__info-list">
           <li class="navigation__info-li">
             <div class="navigation__line"></div>
-            <NuxtLink
-              to="/"
-              class="navigation__info-link"
-            > Careers </NuxtLink>
+            <NuxtLink to="/" class="navigation__info-link"> Careers </NuxtLink>
           </li>
           <li class="navigation__info-li">
             <div class="navigation__line"></div>
-            <NuxtLink
-              to="/"
-              class="navigation__info-link"
-            >
+            <NuxtLink to="/" class="navigation__info-link">
               Education and Training
             </NuxtLink>
           </li>
           <li class="navigation__info-li">
             <div class="navigation__line"></div>
-            <NuxtLink
-              to="/"
-              class="navigation__info-link"
-            > About Us </NuxtLink>
+            <NuxtLink to="/" class="navigation__info-link"> About Us </NuxtLink>
           </li>
           <li class="navigation__info-li">
             <div class="navigation__line"></div>
-            <NuxtLink
-              to="/"
-              class="navigation__info-link"
-            >
+            <NuxtLink to="/" class="navigation__info-link">
               Contact Us
             </NuxtLink>
             <div class="navigation__line"></div>
