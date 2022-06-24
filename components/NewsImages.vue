7673fc3c86879afff8cda7e6a26706f5545e5c84
Valeriy <valeriy.gnybidenko@gmail.com>
2022-06-24 09:53:32 +0300

    feature: complete internal-news page

--- components/NewsImages.vue
+++ components/NewsImages.vue
@@ -6,7 +6,7 @@
           <p class="news-images__name">News name</p>
           <p class="news-images__date">03.06.22</p>
         </div>
-        <img class="news-images__img" src="/images/news/1.jpg" alt="Building" />
+        <img class="news-images__img" src="/images/2.jpg" alt="Building" />
         <p class="news-images__desc">
           Dextall's purpose: To accelerate decarbonization of the environment
           TODAY by bringing affordable and efficient solutions to build
@@ -20,7 +20,7 @@
           <p class="news-images__name">News name</p>
           <p class="news-images__date">03.06.22</p>
         </div>
-        <img class="news-images__img" src="/images/news/2.jpg" alt="Building" />
+        <img class="news-images__img" src="/images/3.jpg" alt="Building" />
         <p class="news-images__desc">
           Elementum tellus augue auctor dolor. Hendrerit nec eget hac in nunc,
           non ut integer netus. Pulvinar sit gravida quam cras eget quisque
@@ -34,7 +34,7 @@
           <p class="news-images__name">News name</p>
           <p class="news-images__date">03.06.22</p>
         </div>
-        <img class="news-images__img" src="/images/news/3.jpg" alt="Building" />
+        <img class="news-images__img" src="/images/4.jpg" alt="Building" />
         <p class="news-images__desc">
           Placerat tincidunt nisi ullamcorper varius feugiat ultrices dapibus
           vel. Leo a orci convallis id. Nec nunc vehicula risus, dictumst donec
@@ -48,7 +48,7 @@
           <p class="news-images__name">News name</p>
           <p class="news-images__date">03.06.22</p>
         </div>
-        <img class="news-images__img" src="/images/news/4.jpg" alt="Building" />
+        <img class="news-images__img" src="/images/5.jpg" alt="Building" />
         <p class="news-images__desc">
           Eget ut eu eget sed in quis dictum posuere. Pellentesque ut ante
           facilisis magna feugiat elementum. Sagittis aliquet urna vel cras
@@ -62,7 +62,7 @@
           <p class="news-images__name">News name</p>
           <p class="news-images__date">03.06.22</p>
         </div>
-        <img class="news-images__img" src="/images/news/5.jpg" alt="Building" />
+        <img class="news-images__img" src="/images/6.jpg" alt="Building" />
         <p class="news-images__desc">
           Sagittis dui et magna bibendum donec sed natoque. Vestibulum, arcu
           enim tellus fermentum lacus. A, nulla quis risus dignissim lorem
@@ -76,7 +76,7 @@
           <p class="news-images__name">News name</p>
           <p class="news-images__date">03.06.22</p>
         </div>
-        <img class="news-images__img" src="/images/news/6.jpg" alt="Building" />
+        <img class="news-images__img" src="/images/7.jpg" alt="Building" />
         <p class="news-images__desc">
           Consectetur purus phasellus quisque et ullamcorper. Urna, etiam nec
           eget enim tempor. Est et enim cursus scelerisque felis euismod. Sit
