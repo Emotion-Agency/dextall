9007b80107c5a35a0f1e847492649479a119f782
letmewin22 <lkosteckiy5@gmail.com>
2022-06-29 15:02:09 +0300

    feature: add AppImg component

---
+++ components/AppImg.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="img-parallax-wrapper"
+    data-parallax="0.06"
+  >
+    <div
+      class="img parallax-img"
+      :class="imgClass"
+      data-parallax="0.06"
+      data-scale="1.07"
+      data-pli
+      :style="{ backgroundImage: `url(${url})` }"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface iProps {
+  url: string,
+  imgClass?: string
+}
+
+defineProps<iProps>()
+</script>
