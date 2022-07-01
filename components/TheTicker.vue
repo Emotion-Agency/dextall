fcec52650bd5ecf336334c50d8a95561bc3f3d05
letmewin22 <lkosteckiy5@gmail.com>
2022-07-01 11:18:38 +0300

    fix: change ticker name

--- components/TheTicker.vue
+++ components/TheTicker.vue
@@ -44,7 +44,7 @@ const repeatNumber = computed(() => {
 })
 
 const initTicker = async () => {
-  const { Ticker } = await import('~/scripts/ticker')
+  const { Ticker } = await import('~/scripts/Ticker')
   ticker = new Ticker({
     $el: $ticker.value,
     duration,
