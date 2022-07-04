3cf10926e7cb195f0f493276cf4d889078a885f4
letmewin22 <lkosteckiy5@gmail.com>
2022-07-04 19:00:31 +0300

    fix: update fonts

--- composables/fonts.ts
+++ composables/fonts.ts
@@ -1,14 +1,14 @@
 const fontsMetaList = [
   {
-    type: 'font/woff2',
-    href: '/fonts/HelveticaNeue.woff2',
+    type: 'font/woff',
+    href: '/fonts/HelveticaNeue.woff',
     crossorigin: 'anonymous',
     as: 'font',
   },
   {
     rel: 'preload',
-    type: 'font/woff2',
-    href: '/fonts/HelveticaNeueItalic.woff2',
+    type: 'font/woff',
+    href: '/fonts/HelveticaNeueItalic.woff',
     crossorigin: 'anonymous',
     as: 'font',
   },
