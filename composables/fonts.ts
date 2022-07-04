503884257d058251b1504ef42c2362bc8dd101f9
letmewin22 <lkosteckiy5@gmail.com>
2022-07-04 19:30:17 +0300

    fix: add woff and woff2 fonts

--- composables/fonts.ts
+++ composables/fonts.ts
@@ -1,5 +1,18 @@
 const fontsMetaList = [
   {
+    type: 'font/woff2',
+    href: '/fonts/HelveticaNeue.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+  {
+    rel: 'preload',
+    type: 'font/woff2',
+    href: '/fonts/HelveticaNeueItalic.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+  {
     type: 'font/woff',
     href: '/fonts/HelveticaNeue.woff',
     crossorigin: 'anonymous',
