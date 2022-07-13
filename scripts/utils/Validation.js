e61c25c71cf0dfa2635c21dbec3272909466d75c
letmewin22 <lkosteckiy5@gmail.com>
2022-07-13 15:48:19 +0300

    fix: fix email validator

--- scripts/utils/Validation.js
+++ scripts/utils/Validation.js
@@ -1,3 +1,6 @@
+const emailTester =
+  /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
+
 class Validation {
   required(str) {
     return !!str.trim()
@@ -10,12 +13,29 @@ class Validation {
     return true
   }
 
-  email(str) {
-    const regExp = /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/
-    const isEmailValid = regExp.test(str.trim())
-    if (!isEmailValid) {
+  email(email) {
+    if (!email) return false
+
+    const emailParts = email.split('@')
+
+    if (emailParts.length !== 2) return false
+
+    const account = emailParts[0]
+    const address = emailParts[1]
+
+    if (account.length > 64) return false
+    else if (address.length > 255) return false
+
+    const domainParts = address.split('.')
+    if (
+      domainParts.some(function (part) {
+        return part.length > 63
+      })
+    )
       return false
-    }
+
+    if (!emailTester.test(email)) return false
+
     return true
   }
 
