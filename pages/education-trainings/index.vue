2d4735f752fa1453388cb55ad73f71586f450c6b
letmewin22 <lkosteckiy5@gmail.com>
2022-09-03 10:52:08 +0300

    fix: update education page, fix form fields to all required

--- pages/education-trainings/index.vue
+++ pages/education-trainings/index.vue
@@ -6,10 +6,37 @@ useObserver('.section')
 
 
 const { story } = await useLibraryStory()
+const { open: openPopup } = useFormPopup()
+
 
 
 
 const date = story.value.first_published_at || story.value.created_at
+
+const onRegistration = (e: Event) => {
+  const el = e.currentTarget as HTMLElement
+  console.log(el.dataset)
+  if (el.dataset.action === 'webinar_reg_form') {
+
+    e.preventDefault()
+    openPopup()
+  }
+}
+
+onMounted(() => {
+  const $content = document.querySelector('.internal-news-2__content')
+  const $btns = $content.querySelectorAll('.circle-button')
+  $btns.forEach(el => {
+    el.addEventListener('click',onRegistration)
+  })
+})
+onBeforeUnmount(() => {
+  const $content = document.querySelector('.internal-news-2__content')
+  const $btns = $content.querySelectorAll('.circle-button')
+  $btns.forEach(el => {
+    el.removeEventListener('click',onRegistration)
+  })
+})
 
 </script>
   
@@ -25,6 +52,12 @@ const date = story.value.first_published_at || story.value.created_at
       :image="story.content.big_image.filename"
       :blocks="story.content.blog_section"
     />
+    <teleport to='body'>
+      <FormPopup
+        title="Registration request"
+        form-title="EDUCATION & TRAINING Registration"
+      />
+    </teleport>
   </main>
 </template>
   
