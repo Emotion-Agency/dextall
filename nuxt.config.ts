84cdb7a56039cb581ee201208d8b0a366e4becc8
letmewin22 <lkosteckiy5@gmail.com>
2022-10-17 11:58:50 +0300

    chore: move storyblok token to env variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,8 +7,13 @@ export default defineNuxtConfig({
   modules: ['@pinia/nuxt', '@nuxtjs/eslint-module'],
 
   buildModules: [
-    ['@storyblok/nuxt', { accessToken: 'zhpJSXvvtqH1uucLMex7UQtt' }],
+    ['@storyblok/nuxt', { accessToken: process.env.STORYBLOK_TOKEN }],
   ],
+
+  publicRuntimeConfig: {
+    STORYBLOK_TOKEN: process.env.STORYBLOK_TOKEN,
+    ENVIROMENT: process.env.NODE_ENV,
+  },
 
   generate: { fallback: '404.html' },
 })
