48dbfbd95f3f4a5b943b7ad2c7c44b48ce71c00e
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-06-07 08:57:11 +0300

    feat: image-list component updated

--- components/ProjectListItem.vue
+++ components/ProjectListItem.vue
@@ -1,4 +1,4 @@
-<script lang='ts' setup>
+<script lang="ts" setup>
 import { iImage } from '~/types/story'
 
 interface iProps {
@@ -9,62 +9,59 @@ interface iProps {
   slug?: string
 }
 
+const infoList = [
+  {
+    text: 'SIZE, SF:',
+    quantity: '282828',
+  },
+  {
+    text: 'SIZE, SF:',
+    quantity: '282828',
+  },
+  {
+    text: 'SIZE, SF:',
+    quantity: '282828',
+  },
+]
+
 defineProps<iProps>()
 </script>
 
 <template>
   <li class="image-list__li">
-    <NuxtLink
-      :to="`/projects/${slug}/`"
-      class="image-list__link"
-    >
-      <div class="image-list__text">
-        <h4 class="image-list__title">{{ title }}</h4>
-        <div class="image-list__desc-wrapper">
-          <p class="image-list__number">(00{{ number }})</p>
-          <p class="image-list__desc">
-            {{ description }}
-          </p>
+    <NuxtLink :to="`/projects/${slug}/`" class="image-list__link">
+      <div class="image-list__top-wrapper">
+        <div class="image-list__img-wrapper">
+          <ParallaxImg
+            :key="images[0].image._uid"
+            :src="images[0].image.filename"
+            :width="457"
+            :height="365"
+            :scale="1.2"
+            class="image-list__img"
+          />
         </div>
+        <h4 class="image-list__title">{{ title }}</h4>
+        <p class="image-list__desc">
+          {{ description }}
+        </p>
       </div>
-      <div
-        v-if="images.length >= 4"
-        class="grid image-list__img-wrapper"
-      >
-        <ParallaxImg
-          :key="images[0].image._uid"
-          :src="images[0].image.filename"
-          :width="457"
-          :height="365"
-          :scale="1.2"
-          class="image-list__img"
-        />
-        <ParallaxImg
-          :key="images[1].image._uid"
-          :src="images[1].image.filename"
-          :width="816"
-          :height="737"
-          :scale="1.2"
-          class="image-list__img"
-        />
-        <ParallaxImg
-          :key="images[2].image._uid"
-          :src="images[2].image.filename"
-          :width="449"
-          :height="365"
-          :scale="1.2"
-          class="image-list__img"
-        />
-        <ParallaxImg
-          :key="images[3].image._uid"
-          :src="images[3].image.filename"
-          :width="460"
-          :height="400"
-          :scale="1.2"
-          class="image-list__img"
-        />
+      <div class="image-list__bottom-wrapper">
+        <ul class="image-list__info">
+          <li
+            v-for="(el, idx) in infoList"
+            :key="idx"
+            class="image-list__info-wrapper"
+          >
+            <div class="image-list__line" />
+            <div class="image-list__info-content">
+              <p class="image-list__text">{{ el.text }}</p>
+              <p class="image-list__quantity">{{ el.quantity }}</p>
+            </div>
+            <div v-if="idx === infoList.length - 1" class="image-list__line" />
+          </li>
+        </ul>
       </div>
     </NuxtLink>
   </li>
-
 </template>
